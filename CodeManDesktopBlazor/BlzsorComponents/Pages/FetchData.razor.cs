2e6f47a57091deaa2f3e996de904731b00af2448
xielinggen <linggen.xie@yintech.cn>
2021-06-03 17:21:13 +0800

    add base ui show

---
+++ CodeManDesktopBlazor/BlzsorComponents/Pages/FetchData.razor.cs
@@ -0,0 +1,42 @@
+﻿using CodeManDesktopBlazor.BlzsorComponents.Models;
+using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeManDesktopBlazor.BlzsorComponents.Pages
+{
+    public partial class FetchData : ComponentBase
+    { 
+        protected override async Task OnInitializedAsync()
+        {
+            try
+            {
+                //HttpClient Http = new HttpClient();
+                //forecasts = await Http.GetFromJsonAsync<WeatherForecast[]>("sample-data/weather.json");
+
+                var lists = new List<WeatherForecast>();
+                for (int i = 0; i < 20; i++)
+                {
+                    lists.Add(new WeatherForecast()
+                    {
+                        Date = DateTime.Now.AddHours(i),
+                        Summary = $"{i}Summary",
+                        TemperatureC = i
+                    });
+                }
+                this.forecasts = lists.ToArray();
+                await Task.Delay(1);
+            }
+            catch (Exception ex)
+            {
+                this.errormsg = ex.Message;
+            }
+        }
+
+        
+    }
+}
--- CodeManDesktopBlazor/BlzsorComponents/Shared/NavMenu.razor.cs
+++ CodeManDesktopBlazor/BlzsorComponents/Shared/NavMenu.razor.cs
@@ -4,9 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace CodeManDesktopBlazor.BlzsorComponents
+namespace CodeManDesktopBlazor.BlzsorComponents.Shared
 {
-    public partial class CounterDemo
+    public partial class NavMenu
     {
     }
 }
